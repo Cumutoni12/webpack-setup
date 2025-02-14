8223dc2c13e9aed7789b8ba23161e511b32cdc17
Cumutoni12 <cumutoni@gmail.com>
2025-02-14 15:52:25 -0500

    updated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-imp
+import "./index.css";
+import { add } from "./modules/add";
+add(11, 12, 13);
--- src/modules/add.js
+++ src/modules/add.js
@@ -1,5 +1,5 @@
-
-export function add (num1,num2,num3){
-    const sum=num1+num2+num3
-
+export function add(num1, num2, num3) {
+  const sum = num1 + num2 + num3;
+  console.log(sum);
 }
+
